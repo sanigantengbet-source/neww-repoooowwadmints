5eac9e5f68efd24ff51f829a453fd7d7425a4df5
ALOK GANTENG <sanigantengbet@gmail.com>
2026-09-08 00:14:13 +0700

    Upload app/admin/tools/new/page.tsx

---
+++ app/admin/tools/new/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ToolForm from '@/components/admin/ToolForm';
+
+export default function NewToolPage() {
+  return (
+    <div>
+      <ToolForm isEdit={false} />
+    </div>
+  );
+}
